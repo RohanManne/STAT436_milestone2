296f3b16b57b8c7bc20e911100518fdf930ba508
RohanSM123 <68923023+RohanSM123@users.noreply.github.com>
2024-11-01 14:18:29 -0500

    Added world map

--- Milestone2.R
+++ Milestone2.R
@@ -1,54 +1,110 @@
 library(shiny)
-library(countrycode)
 library(tidyverse)
-energy <- read.csv("./owid-energy.csv")
-energy_long <- energy |> pivot_longer(c("biofuel_electricity", "coal_electricity", "fossil_electricity", "gas_electricity", "hydro_electricity", "low_carbon_electricity", "nuclear_electricity", "oil_electricity", "other_renewable_electricity", "renewables_electricity", "solar_electricity", "wind_electricity"), names_to= "energy_type", values_to = "energy_values")
-energy_long <- energy_long |> 
-  mutate(continent = countrycode(country, origin="country.name", destination="continent"), energy_type = str_to_title(str_replace_all(energy_type, "_", " "))) |>
-  filter(continent != "NA" & continent != "Antarctica" & energy_values != 0) 
+library(plotly)
+library(countrycode)
 
+energy <- read.csv("owid-energy.csv")
+renewable_share <- read.csv("renewable-share-energy.csv")
 
-facet_data <- function (){
-  energy_long |>
-    ggplot() + 
-    geom_boxplot(aes(energy_values, energy_type, col=continent)) + 
+# data for choropleth map
+renewable_share <- renewable_share %>%
+  rename(Country = Entity, Code = Code, Year = Year, 
+         Renewable_Percentage = Renewables) %>%
+  mutate(continent = countrycode(Country, origin = "country.name", destination = "continent"))
+
+energy_long <- energy %>%
+  pivot_longer(
+    c("biofuel_electricity", "coal_electricity", "fossil_electricity", "gas_electricity", 
+      "hydro_electricity", "low_carbon_electricity", "nuclear_electricity", "oil_electricity", 
+      "other_renewable_electricity", "renewables_electricity", "solar_electricity", "wind_electricity"),
+    names_to = "energy_type",
+    values_to = "energy_values"
+  ) %>%
+  mutate(
+    continent = countrycode(country, origin = "country.name", destination = "continent"),
+    energy_type = str_to_title(str_replace_all(energy_type, "_", " "))
+  ) %>%
+  filter(continent != "NA" & continent != "Antarctica" & energy_values != 0)
+
+facet_data <- function() {
+  ggplot(energy_long) + 
+    geom_boxplot(aes(energy_values, energy_type, color = continent)) + 
     scale_x_log10() +
-    facet_wrap(.~continent) +
+    facet_wrap(. ~ continent) +
     theme_bw() + 
-    xlab("Electricity Generation (Terawatt-Hours)")+
+    xlab("Electricity Generation (Terawatt-Hours)") +
     ylab("Energy Type") +
-    theme(axis.text.x = element_text(angle=45)) + 
+    theme(axis.text.x = element_text(angle = 45)) + 
     ggtitle("Electricity Generation vs Energy Type, Faceted by Continent")
 }
 
-scatter_data <- function (){
-  energy_long %>%
-    ggplot() +
-    geom_line(aes(year, renewables_consumption, 
-                  col = continent, group = country),
-              size = 0.7, alpha = 0.7) +
+scatter_data <- function() {
+  ggplot(energy_long) +
+    geom_line(aes(year, renewables_consumption, color = continent, group = country), size = 0.7, alpha = 0.7) +
     scale_y_log10() +
     scale_x_continuous(expand = c(0, 0)) +
     xlab("Year") +
-    ylab("Ren. Energy Consumption (terawatt-hours, log10 scale)") +
+    ylab("Renewable Energy Consumption (terawatt-hours, log10 scale)") +
     ggtitle("Renewable Energy Consumption by Year")
 }
 
 ui <- fluidPage(
-  h1("Shiny Energy Data Visualization"),
-  plotOutput("plot"),
-  h3(""), #new line between graphs
-  plotOutput("scatter")
+  titlePanel("Renewable Energy Analysis by Country"),
+  sidebarLayout(
+    sidebarPanel(
+      selectInput("year", "Select Year:", choices = sort(unique(renewable_share$Year)), selected = 1965)
+    ),
+    mainPanel(
+      plotlyOutput("choropleth_map"),
+      plotOutput("plot"),
+      plotOutput("scatter")
+    )
+  )
 )
 
-server <- function(input, output){
+server <- function(input, output, session) {
+  
+  # choropleth map using Plotly
+  output$choropleth_map <- renderPlotly({
+    selected_data <- renewable_share %>% filter(Year == input$year)
+    
+    all_countries <- unique(countrycode::codelist$iso3c)
+    full_data <- tibble(Code = all_countries) %>%
+      left_join(selected_data, by = "Code") %>%
+      mutate(Display_Value = ifelse(is.na(Renewable_Percentage), -1, Renewable_Percentage))
+    
+    # custom color scale where -1 (NA) is gray
+    custom_colorscale <- list(
+      list(0, "gray"),
+      list(0.0001, "lightblue"),
+      list(1, "darkblue")
+    )
+    
+    max_percent <- max(full_data$Renewable_Percentage, na.rm = TRUE)
+    
+    plot_ly(data = full_data, type = 'choropleth', locations = ~Code,
+            z = ~Display_Value, text = ~paste0(Country, "<br>Year: ", input$year, "<br>Renewable %: ", ifelse(is.na(Renewable_Percentage), "N/A", Renewable_Percentage)),
+            colorscale = custom_colorscale, zmin = 0, zmax = max_percent,
+            marker = list(line = list(color = 'white', width = 0.5))) %>%
+      colorbar(title = "Renewable Energy %") %>%
+      layout(
+        geo = list(
+          showframe = FALSE,
+          showcoastlines = FALSE,
+          projection = list(type = 'equirectangular'),
+          bgcolor = "white"
+        ),
+        title = paste("Renewable Energy Percentage by Country in", input$year)
+      )
+  })
+  
   output$plot <- renderPlot({
     facet_data()
   })
+  
   output$scatter <- renderPlot({
     scatter_data()
   })
 }
 
-app <- shinyApp(ui, server)
-app
+shinyApp(ui, server)
